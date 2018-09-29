9323be2743bb5e0c967b806efc76c2f760568d53
konstantinrjab <konstantinrjab@gmail.com>
2018-09-29 16:42:14 +0300

    add city

--- src/app/place/place-add.component.ts
+++ src/app/place/place-add.component.ts
@@ -1,8 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {PlaceService} from '../place.service';
-import {HttpErrorResponse} from '@angular/common/http';
-import {forEach} from '@angular/router/src/utils/collection';
+import {WeatherService} from '../weather.service';
 
 @Component({
   selector: 'app-place-add',
@@ -11,9 +10,11 @@ import {forEach} from '@angular/router/src/utils/collection';
 })
 export class PlaceAddComponent implements OnInit {
   protected townNameModel;
-  error;
+  error: Array<string>;
+  success: string;
 
-  constructor(private placeService: PlaceService) {
+  constructor(private placeService: PlaceService,
+              private weatherService: WeatherService) {
   }
 
   ngOnInit() {
@@ -22,21 +23,30 @@ export class PlaceAddComponent implements OnInit {
   addTown(placeForm: NgForm): void {
     console.log(placeForm.value);
     if (!this.error) {
-      this.placeService.addTown(placeForm.value)
+      this.weatherService.getWeather(this.townNameModel)
         .subscribe((res) => {
-            console.log(res);
+            this.placeService.addTown(placeForm.value)
+              .subscribe((res2) => {
+                this.success = 'Successfully added!';
+                }, error => {
+                  this.handleError(error);
+                },
+              );
           },
           error => {
             this.handleError(error);
           },
         );
     } else {
-      console.log('there was an error');
+      console.error('there was an error');
     }
   }
 
   handleError(error) {
-    this.error = Object.values(error);
+    if (typeof error === 'object') {
+      error = Object.values(error);
+    }
+    this.error = error;
     console.log(this.error);
   }
 }
